4a0a82dfbe315557755db576552a15d75bd84987
Rui Vilares <rui.vilares@hotmail.com>
2016-11-15 09:54:07 +0000

    Logger + WeightCalculation + Stats

---
+++ src/Util/Logger.java
@@ -0,0 +1,29 @@
+package Util;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Logger {
+	private static File logFile = new File("logFile.txt");
+
+	public synchronized static void writeToLog(String toLog) {
+		FileOutputStream logStream;
+		try {
+			logStream = new FileOutputStream(logFile, true);
+		} catch (FileNotFoundException e1) {
+			System.err.println("LOGGER: Could not log");
+			return;
+		}
+
+		String timeStamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date(System.currentTimeMillis()));
+		try {
+			logStream.write((timeStamp + " - " + toLog + "\n").getBytes());
+		} catch (IOException e) {
+			System.err.println("LOGGER: Could not write to log.");
+		}
+	}
+}
--- src/serviceConsumerProviderVis/Person.java
+++ src/serviceConsumerProviderVis/Person.java
@@ -5,10 +5,10 @@ import repast.simphony.random.RandomHelper;
 public class Person {
 	private double weight;
 	private int destination;
-	
+
 	public Person(int destination){
 		this.destination = destination;
-		this.weight = RandomHelper.nextDoubleFromTo(40, 200);
+		this.weight = generateWeight();
 	}
 
 	public double getWeight() {
@@ -17,6 +17,19 @@ public class Person {
 
 	public int getDestination() {
 		return destination;
-	}	
-	
+	}
+
+	private double generateWeight(){
+		double[] weightArray = new double[10];
+		weightArray[0] = RandomHelper.nextDoubleFromTo(40, 60);
+		for (int i = 1; i < 6; i++){
+			weightArray[i] = RandomHelper.nextDoubleFromTo(60, 80);
+		}
+		for (int i = 6; i < 9; i++){
+			weightArray[i] = RandomHelper.nextDoubleFromTo(80, 100);
+		}
+		weightArray[9] = RandomHelper.nextDoubleFromTo(100, 150);
+		return weightArray[RandomHelper.nextIntFromTo(0, 9)];
+	}
+
 }
